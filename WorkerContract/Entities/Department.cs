7ee2d02379a5fa72fba996380eb0a61239875dac
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-02-18 17:49:45 -0300

    My first commmit

---
+++ WorkerContract/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerContract.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string deptName)
+        {
+            Name = deptName;
+        }
+
+    }
+}
