4946cf9818fa8fd00a74a5f13f1d5c36857861a5
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-02-19 15:15:04 -0300

    Reestruct of baseline and add Generics project

---
+++ GenericsApp/GenericsApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using GenericsApp.Service;
+using GenericsApp.Entities;
+
+namespace GenericsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            CalculationService calculationService = new CalculationService();
+
+            for (int index = 1; index <= n; index++)
+            {
+                Console.Write("Enter a value of a product (name,value): ");
+                Product prod = new Product(Console.ReadLine());
+
+                list.Add(prod);
+            }
+
+            Console.WriteLine("Max value: " + calculationService.Max(list));
+        }
+    }
+}
---
+++ GenericsApp/GenericsApp/Service/CalculationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsApp.Service
+{
+    class CalculationService
+    {
+        public EspecificType Max<EspecificType>(List<EspecificType> list) where EspecificType : IComparable
+        {
+            EspecificType max = list[0];
+
+            foreach (EspecificType e in list)
+            {
+                if (e.CompareTo(max) > 0)
+                {
+                    max = e;
+                }
+            }
+            return max;
+        }
+    }
+}
