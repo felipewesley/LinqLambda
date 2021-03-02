6437c48dfdc5fed3f41971de082db4e1f4254694
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-03-02 12:15:31 -0300

    Aprendendo sobre operador Linq e expressoes Lambda

---
+++ LinqLambda/LinqLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
